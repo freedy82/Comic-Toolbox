6620c172df2e9b4a5d81ff9523d66a3e850df470
freedy82 <freedy82@hotmail.com>
2022-04-30 11:49:00 +0800

    Delete make_exe.bat

--- make_exe.bat
+++
@@ -1,9 +0,0 @@
-@echo --clean --disable-windowed-traceback
-SET PYTHONDONTWRITEBYTECODE=1
-pyinstaller -F start.py -i ./uis/resources/icon.ico --add-data="models/sites/*;models/sites/" --add-data="models/readers/*;models/readers/" -w --name ComicToolbox --python-option B
-@mkdir dist\languages\
-@copy /Y languages\*.qm dist\languages\
-@mkdir dist\themes\
-@copy /Y themes\*.qss dist\themes\
-@del /F /Q dist\themes\base.qsst
-SET PYTHONDONTWRITEBYTECODE=0
